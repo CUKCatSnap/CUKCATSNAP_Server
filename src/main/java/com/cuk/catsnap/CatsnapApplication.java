db2a8f5924a057693de2f9a053208ebedb36b584
redblackblossom <physicsdev1029@gmail.com>
2024-09-22 10:53:23 +0900

    Jenkins test commit!

---
+++ src/main/java/com/cuk/catsnap/CatsnapApplication.java
@@ -0,0 +1,13 @@
+package com.cuk.catsnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsnapApplication.class, args);
+	}
+
+}
