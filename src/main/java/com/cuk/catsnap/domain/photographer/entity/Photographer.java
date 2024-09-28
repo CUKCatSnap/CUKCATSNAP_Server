1780751229d5a050e529da7cd1e92f0fe8b9d589
redblackblossom <physicsdev1029@gmail.com>
2024-09-28 14:01:35 +0900

    [FEAT] adding photographere-related entity

---
+++ src/main/java/com/cuk/catsnap/domain/photographer/entity/Photographer.java
@@ -0,0 +1,33 @@
+package com.cuk.catsnap.domain.photographer.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="photograph")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Photographer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String identifier;
+    private String password;
+    private String nickname;
+    private LocalDate birthday;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "profile_photo")
+    private String profilePhoto;
+}
