c0dc48d40934d81120ceb8faa8b8f068a783832c
redblackblossom <physicsdev1029@gmail.com>
2024-10-02 11:29:50 +0900

    [FEAT] add page format to ReservationController

--- src/main/java/com/cuk/catsnap/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/cuk/catsnap/domain/reservation/controller/ReservationController.java
@@ -1,6 +1,7 @@
 package com.cuk.catsnap.domain.reservation.controller;
 
 import com.cuk.catsnap.domain.reservation.dto.ReservationResponse;
+import com.cuk.catsnap.global.result.PagedData;
 import com.cuk.catsnap.global.result.ResultResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -23,11 +24,11 @@ public class ReservationController {
 
     @Operation(summary = "예약을 조회하는 API", description = "예약을 조회하는 API입니다. 쿼리 파라미터 type으로 적절한 유형의 예약을 조회")
     @ApiResponses({
-            @ApiResponse(responseCode = "200 SR000", description = "성공적으로 예약을 조회했습니다.", content = @Content(schema = @Schema(implementation = ReservationResponse.MyReservationList.class))),
+            @ApiResponse(responseCode = "200 SR000", description = "성공적으로 예약을 조회했습니다."),
             @ApiResponse(responseCode = "400 SR000", description = "예약 조회 실패", content = @Content(schema = @Schema(implementation = ResultResponse.class))),
     })
     @GetMapping("/my")
-    public ResultResponse<ReservationResponse.MyReservationList> getMyReservation(
+    public ResultResponse<PagedData<ReservationResponse.MyReservationList>> getMyReservation(
             @Parameter(description="all : 내 모든 예약(정렬 : 최근 예약한 시간 느릴수록 먼저옴) upcoming : 미래에 시작하는 예약(정렬 : 미래 예약 중 현재와 가까운 것이 먼저옴) ")
             @RequestParam("type")
             String reservationQuery,
