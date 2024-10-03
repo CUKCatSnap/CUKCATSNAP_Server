d2b1bf4653262002e201fb02e87857eee49fc5bf
redblackblossom <physicsdev1029@gmail.com>
2024-10-03 12:56:46 +0900

    [FIX] 예약 프로그램 조회 형식에 소요 시간 항목 추가

--- src/main/java/com/cuk/catsnap/domain/reservation/dto/ReservationResponse.java
+++ src/main/java/com/cuk/catsnap/domain/reservation/dto/ReservationResponse.java
@@ -121,6 +121,7 @@ public class ReservationResponse {
         private String title;
         private String content;
         private Long price;
+        private Long durationTimeMinute;
     }
 
     @Getter
