98c3ea9f1d71b61494952d534611cceb97b1be49
redblackblossom <physicsdev1029@gmail.com>
2024-10-04 20:10:35 +0900

    [FEAT] 장소 구독 구독 취소, 작가 구독 구독취소, 작가 차단 차단 해제 API 명세 작성

--- src/main/java/com/cuk/catsnap/domain/social/controller/SocialController.java
+++ src/main/java/com/cuk/catsnap/domain/social/controller/SocialController.java
@@ -9,9 +9,12 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.print.Pageable;
@@ -23,7 +26,7 @@ public class SocialController {
 
     @Operation(summary = "구독한 작가 목록 또는 차단한 작가 목록 조회", description = "구독과 차단한 작가를 조회하는 API입니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200 SS000", description = "성공적으로 구독 목록을 조회했습니다."),
+            @ApiResponse(responseCode = "200 So000", description = "성공적으로 구독 목록을 조회했습니다."),
     })
     @GetMapping("/{type}/photographer")
     public ResultResponse<PagedData<PhotographerResponse.PhotographerTinyInformationList>> getSubscribePhotographerList(
@@ -38,7 +41,7 @@ public class SocialController {
 
     @Operation(summary = "구독한 장소 목록 조회", description = "구독한 장소의 목록을 조회하는 API입니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200 SS000", description = "성공적으로 구독 목록을 조회했습니다."),
+            @ApiResponse(responseCode = "200 So000", description = "성공적으로 구독 목록을 조회했습니다."),
     })
     @GetMapping("/subscribe/place")
     public ResultResponse<PagedData<SocialResponse.subscribePlaceList>> getSubscribePlaceList(
@@ -48,4 +51,55 @@ public class SocialController {
         return null;
     }
 
+    @Operation(summary = "특정 작가를 구독하거나 구독 취소하는 API", description = "특정 작가를 구독하거나 구독을 취소하는 API입니다. 이미 구독중이면 구독을 취소하고, 구독중이 아니면 구독합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200 So001", description = "작가 구독(구독 취소)를 토글 했습니다."),
+    })
+    @PostMapping("subscribe/photographer/{photographerId}")
+    public ResultResponse<?> photographerSubscribeToggle(
+            @Parameter(description = "구독을 토글할 작가의 id")
+            @PathVariable
+            Long photographerId
+    ){
+        return null;
+    }
+
+    @Operation(summary = "특정 작가를 차단하거나 차단 취소하는 API", description = "특정 작가를 차단하거나 차단을 취소하는 API입니다. 이미 차단중이면 차단을 취소하고, 차단중이 아니면 차단합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200 So002", description = "작가 차단(차단 취소)를 토글 했습니다."),
+    })
+    @PostMapping("block/photographer/{photographerId}")
+    public ResultResponse<?> photographerBlockTogglePhotographer(
+            @Parameter(description = "차단을 토글할 작가의 id")
+            @PathVariable
+            Long photographerId
+    ){
+        return null;
+    }
+
+    @Operation(summary = "특정 장소를 구독하는 API", description = "특정 장소를 구독할 수 있는 API입니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200 So003", description = "장소 구독을 했습니다."),
+    })
+    @PostMapping("/subscribe/place")
+    public ResultResponse<?> placeSubscribe(
+            @Parameter(description = "구독할 장소")
+            @RequestParam("keyword")
+            String keyword
+    ){
+        return null;
+    }
+
+    @Operation(summary = "특정 장소의 구독을 취소하는 API", description = "특정 장소의 구독을 취소할 수 있는 API입니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200 So004", description = "장소 구독을 취소했습니다."),
+    })
+    @DeleteMapping("/subscribe/place")
+    public ResultResponse<?> placeSubscribeCancel(
+            @Parameter(description = "구독을 취소할 장소의 Id")
+            @RequestParam("subscribePlaceId")
+            Long subscribePlaceId
+    ){
+        return null;
+    }
 }
