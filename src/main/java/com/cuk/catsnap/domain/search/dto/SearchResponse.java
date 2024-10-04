8cf35df1b2c753df9f90e7f7b97114c5601c3dea
redblackblossom <physicsdev1029@gmail.com>
2024-10-04 12:31:48 +0900

    [FEAT] 피드와 리뷰 상세 페이지 응답 API 스펙

--- src/main/java/com/cuk/catsnap/domain/search/dto/SearchResponse.java
+++ src/main/java/com/cuk/catsnap/domain/search/dto/SearchResponse.java
@@ -1,10 +1,14 @@
 package com.cuk.catsnap.domain.search.dto;
 
+import com.cuk.catsnap.domain.member.dto.MemberResponse;
 import com.cuk.catsnap.domain.photographer.dto.PhotographerResponse;
+import com.cuk.catsnap.domain.reservation.dto.ReservationResponse;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class SearchResponse {
@@ -12,32 +16,125 @@ public class SearchResponse {
     @Getter
     @Builder
     public static class PreviewFeedSearchList{
-        List<PreviewFeedSearch> previewFeedSearchList;
+        private List<PreviewFeedSearch> previewFeedSearchList;
     }
 
     @Getter
     @Builder
     public static class PreviewFeedSearch {
-        public PhotographerResponse.PhotographerFullyInformation photographerTinyInformation;
+        private PhotographerResponse.PhotographerFullyInformation photographerFullyInformation;
+
         @Schema(description = "검색된 피드의 id")
-        public Long feedId;
+        private Long feedId;
+
+        @Schema(description = "위치 정보")
+        private ReservationResponse.Location location;
+
         @Schema(description = "검색에 보여줄 이미지 1장")
-        public String firstPhotoUrl;
+        private String firstPhotoUrl;
+
+        @Schema(description = "글 작성 시간")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime createdAt;
+    }
+
+    @Getter
+    @Builder
+    public static class DetailFeedSearchList {
+        private List<DetailFeedSearch> detailFeedSearchList;
+    }
+
+    @Getter
+    @Builder
+    public static class DetailFeedSearch{
+        private PhotographerResponse.PhotographerFullyInformation photographerFullyInformation;
+
+        @Schema(description = "글 작성 시간")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime createdAt;
+
+        @Schema(description = "위치 정보")
+        private ReservationResponse.Location location;
+
+        @Schema(description = "피드의 내용")
+        private String content;
+
+        @Schema(description = "피드 사진 URL")
+        private List<String> photoURLList;
+
+        @Schema(description = "좋아요 수")
+        private Long likeCount;
+
+        @Schema(description = "내가 좋아요를 눌렀는지")
+        private Boolean isMeLiked;
     }
 
     @Getter
     @Builder
     public static class PreviewReviewSearchList{
-        List<PreviewReviewSearch> previewReviewSearchList;
+        private List<PreviewReviewSearch> previewReviewSearchList;
     }
 
     @Getter
     @Builder
     public static class PreviewReviewSearch {
-        public PhotographerResponse.PhotographerFullyInformation photographerTinyInformation;
+        private PhotographerResponse.PhotographerFullyInformation photographerFullyInformation;
+        private MemberResponse.MemberTinyInformation memberFullyInformation;
+
         @Schema(description = "검색된 리뷰의 id")
-        public Long reviewId;
+        private Long reviewId;
+
+        @Schema(description = "위치 정보")
+        private ReservationResponse.Location location;
+
         @Schema(description = "검색에 보여줄 이미지 1장")
-        public String firstPhotoUrl;
+        private String firstPhotoUrl;
+
+        @Schema(description = "작가에 대한 별점")
+        private Integer photographerRating;
+
+        @Schema(description = "장소에 대한 별점")
+        private Integer placeRating;
+
+        @Schema(description = "글 작성 시간")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime createdAt;
+    }
+
+    @Getter
+    @Builder
+    public static class DetailReviewSearchList {
+        List<DetailReviewSearch> detailReviewSearchList;
+    }
+
+    @Getter
+    @Builder
+    public static class DetailReviewSearch{
+        private PhotographerResponse.PhotographerFullyInformation photographerFullyInformation;
+
+        @Schema(description = "글 작성 시간")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime createdAt;
+
+        @Schema(description = "위치 정보")
+        private ReservationResponse.Location location;
+
+        @Schema(description = "리뷰의 내용")
+        private String content;
+
+        @Schema(description = "리뷰 사진 URL")
+        private List<String> photoURLList;
+
+        @Schema(description = "작가에 대한 별점")
+        private Integer photographerRating;
+
+        @Schema(description = "장소에 대한 별점")
+        private Integer placeRating;
+
+        @Schema(description = "좋아요 수")
+        private Long likeCount;
+
+        @Schema(description = "내가 좋아요를 눌렀는지")
+        private Boolean isMeLiked;
     }
 }
