7631f53960d40a25fcbe9fdc282a1d19fbbcf2cb
redblackblossom <physicsdev1029@gmail.com>
2024-10-12 21:06:17 +0900

    [FEAT] JWT 관련 의존성 추가

--- src/main/java/com/cuk/catsnap/global/security/SecurityFilterChainConfig.java
+++ src/main/java/com/cuk/catsnap/global/security/SecurityFilterChainConfig.java
@@ -1,5 +1,7 @@
 package com.cuk.catsnap.global.security;
 
+import com.cuk.catsnap.global.security.filter.MemberSignInAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,18 +9,18 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    private final MemberSignInAuthenticationFilter memberSignInAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain config(HttpSecurity http) throws Exception {
         http
+            .addFilterAt(memberSignInAuthenticationFilter, BasicAuthenticationFilter.class)
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(authorizeRequests->
                     authorizeRequests
