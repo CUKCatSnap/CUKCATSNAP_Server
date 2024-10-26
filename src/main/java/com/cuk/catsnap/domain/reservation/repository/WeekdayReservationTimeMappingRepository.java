e5f19167fd3c2ceca88a71829d098e2b6b963b16
redblackblossom <physicsdev1029@gmail.com>
2024-10-26 18:48:41 +0900

    [FIX] jpql에 쿼리에서 @Param 에노테이션으로 매핑

--- src/main/java/com/cuk/catsnap/domain/reservation/repository/WeekdayReservationTimeMappingRepository.java
+++ src/main/java/com/cuk/catsnap/domain/reservation/repository/WeekdayReservationTimeMappingRepository.java
@@ -5,6 +5,7 @@ import com.cuk.catsnap.domain.reservation.entity.WeekdayReservationTimeMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,7 +14,7 @@ import java.util.Optional;
 public interface WeekdayReservationTimeMappingRepository extends JpaRepository<WeekdayReservationTimeMapping, Long> {
     @Modifying
     @Query("UPDATE WeekdayReservationTimeMapping m  SET m.reservationTimeFormatId = null WHERE m.photographer.id = :photographerId AND m.reservationTimeFormatId = :reservationTimeFormatId")
-    void updateReservationTimeFormatIdToNull(Long photographerId, String reservationTimeFormatId);
+    void updateReservationTimeFormatIdToNull(@Param("photographerId") Long photographerId, @Param("reservationTimeFormatId") String reservationTimeFormatId);
 
     Optional<WeekdayReservationTimeMapping> findByPhotographerIdAndWeekday(Long photographerId, Weekday weekday);
 }
