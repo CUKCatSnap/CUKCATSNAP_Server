1b4d32f43dd4ff7d05080378337dfe600953862d
redblackblossom <physicsdev1029@gmail.com>
2024-10-30 13:36:36 +0900

    [FEAT] jts 라이브러리의 GeometryFactory와 컨버터 설정

---
+++ src/main/java/com/cuk/catsnap/global/geography/converter/GeographyConverter.java
@@ -0,0 +1,18 @@
+package com.cuk.catsnap.global.geography.converter;
+
+import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GeographyConverter {
+
+    private final GeometryFactory geometryFactory;
+
+    public Point toPoint(Double latitude, Double longitude) {
+        return geometryFactory.createPoint(new Coordinate(longitude, latitude));
+    }
+}
