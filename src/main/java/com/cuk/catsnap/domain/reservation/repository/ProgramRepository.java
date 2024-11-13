f13ba8101f71ee3ca9eb9c0f580ab40b8667de53
redblackblossom <physicsdev1029@gmail.com>
2024-11-13 16:24:29 +0900

    [REFACT] 삭제 로직 서비수에서 수행하도록 리팩토링

--- src/main/java/com/cuk/catsnap/domain/reservation/repository/ProgramRepository.java
+++ src/main/java/com/cuk/catsnap/domain/reservation/repository/ProgramRepository.java
@@ -5,18 +5,10 @@ import com.cuk.catsnap.domain.reservation.entity.Program;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProgramRepository extends JpaRepository<Program, Long> {
-
-    @Modifying
-    @Query("UPDATE Program p SET p.deleted = true WHERE p.id = :programId and p.photographer.id = :photographerId")
-    int softDeleteByProgramIdAndPhotographerId(@Param("programId") Long programId,
-        @Param("photographerId") Long photographerId);
 
     List<Program> findByPhotographerIdAndDeletedFalse(Long photographerId);
 
