ad56fe2e7c4705149b57ac16735b52366742a64d
redblackblossom <physicsdev1029@gmail.com>
2024-11-13 16:01:53 +0900

    [FEAT] 테스트 코드를 위한 Fixture 클래스 작성

---
+++ src/test/java/com/cuk/catsnap/support/fixture/ProgramFixture.java
@@ -0,0 +1,31 @@
+package com.cuk.catsnap.support.fixture;
+
+import com.cuk.catsnap.domain.photographer.entity.Photographer;
+import com.cuk.catsnap.domain.reservation.entity.Program;
+
+public class ProgramFixture {
+
+    private Long id;
+    private Photographer photographer = PhotographerFixture.photographer().build();
+    private String title = "title";
+    private String content = "context";
+    private Long price = 50000L;
+    private Long durationMinutes = 60L;
+    private Boolean deleted = false;
+
+    public static ProgramFixture program() {
+        return new ProgramFixture();
+    }
+
+    public Program build() {
+        return Program.builder()
+            .id(this.id)
+            .photographer(this.photographer)
+            .title(this.title)
+            .content(this.content)
+            .price(this.price)
+            .durationMinutes(this.durationMinutes)
+            .deleted(this.deleted)
+            .build();
+    }
+}
