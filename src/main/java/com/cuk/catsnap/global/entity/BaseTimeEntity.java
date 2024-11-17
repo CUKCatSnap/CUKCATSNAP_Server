09257ed76b49ff1b5435fe3cbc39647009adf4f8
redblackblossom <physicsdev1029@gmail.com>
2024-11-17 18:23:59 +0900

    [FIX] createdAt의 null값 허용

--- src/main/java/com/cuk/catsnap/global/entity/BaseTimeEntity.java
+++ src/main/java/com/cuk/catsnap/global/entity/BaseTimeEntity.java
@@ -15,7 +15,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
     @LastModifiedDate
     @Column(name = "updated_at")
