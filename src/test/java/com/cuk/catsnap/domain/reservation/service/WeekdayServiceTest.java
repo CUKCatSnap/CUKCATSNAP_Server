1ebe6c3037929bc0e54a818f5ea7e6d2c61fcd29
redblackblossom <physicsdev1029@gmail.com>
2024-11-18 13:26:47 +0900

    [FEAT] 특정 날짜가 어떤 요일인지 확인하는 로직에 공휴일 확인 추가

---
+++ src/test/java/com/cuk/catsnap/domain/reservation/service/WeekdayServiceTest.java
@@ -0,0 +1,46 @@
+package com.cuk.catsnap.domain.reservation.service;
+
+import static org.mockito.BDDMockito.given;
+
+import com.cuk.catsnap.domain.reservation.entity.Weekday;
+import java.time.LocalDate;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class WeekdayServiceTest {
+
+    @InjectMocks
+    private WeekdayService weekdayService;
+
+    @Mock
+    private HolidayService holidayService;
+
+    @ParameterizedTest
+    @CsvSource({
+        "2025-01-01, HOLIDAY, true",  // 공휴일
+        "2025-01-02, THURSDAY, false", // 평일(목요일)
+        "2025-01-03, FRIDAY, false"  // 평일(금요일)
+    })
+    void 요일과_공휴일_조회(LocalDate date, Weekday expectedWeekday, boolean isHoliday) {
+        // given
+        given(holidayService.isHoliday(date))
+            .willReturn(isHoliday);
+
+        // when
+        Weekday weekday = weekdayService.getWeekday(date);
+
+        // then
+        Assertions.assertThat(weekday).isEqualTo(expectedWeekday);
+    }
+
+}
