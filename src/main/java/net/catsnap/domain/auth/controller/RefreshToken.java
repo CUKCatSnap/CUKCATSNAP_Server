d649d679215607930eb53d79752328c00a0851eb
redblackblossom <physicsdev1029@gmail.com>
2024-11-30 10:30:53 +0900

    [REFACT] 패키지 명 변경

--- src/main/java/net/catsnap/domain/auth/controller/RefreshToken.java
+++ src/main/java/net/catsnap/domain/auth/controller/RefreshToken.java
@@ -1,10 +1,10 @@
-package net.catsnap.domain.token.controller;
+package net.catsnap.domain.auth.controller;
 
-import net.catsnap.global.result.ResultResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import net.catsnap.global.result.ResultResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
