f1e5b968e55207b211841969675eefa160ce2e71
redblackblossom <physicsdev1029@gmail.com>
2024-12-02 23:42:53 +0900

    [FEAT] 공휴일 도큐먼트에 공휴일 이름 추가

--- src/main/java/net/catsnap/domain/reservation/document/Holiday.java
+++ src/main/java/net/catsnap/domain/reservation/document/Holiday.java
@@ -2,16 +2,22 @@ package net.catsnap.domain.reservation.document;
 
 import jakarta.persistence.Id;
 import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
 @RedisHash(value = "Holiday", timeToLive = 60 * 60 * 24 * 365) // 365일
+@NoArgsConstructor
+@Getter
 public class Holiday {
 
     @Id
     private String id;
 
-    public Holiday(LocalDate date) {
+    private String holidayName;
+
+    public Holiday(LocalDate date, String holidayName) {
         this.id = date.toString();
-        this.id = id;
+        this.holidayName = holidayName;
     }
 }
