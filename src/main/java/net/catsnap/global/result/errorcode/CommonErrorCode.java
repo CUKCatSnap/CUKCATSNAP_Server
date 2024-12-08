c568eca9fb3780096e947f90b5a9eb090fdeed15
redblackblossom <physicsdev1029@gmail.com>
2024-12-08 13:56:24 +0900

    [FEAT] 필수값인 Query String  없을 때 예외 처리

--- src/main/java/net/catsnap/global/result/errorcode/CommonErrorCode.java
+++ src/main/java/net/catsnap/global/result/errorcode/CommonErrorCode.java
@@ -8,6 +8,7 @@ import net.catsnap.global.result.ResultCode;
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ResultCode {
     NOT_FOUND_API(404, "EC000", "존재하지 않는 API 입니다."),
+    MISSING_REQUEST_PARAMETER(400, "EC001", "필수 쿼리 파라미터가 누락되었습니다."),
     ;
 
     private final int status;
