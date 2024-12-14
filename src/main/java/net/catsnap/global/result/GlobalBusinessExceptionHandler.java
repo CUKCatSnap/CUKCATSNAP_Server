b8990506064d39e23bb4bca987da7fe519be4926
redblackblossom <physicsdev1029@gmail.com>
2024-12-14 22:14:00 +0900

    [FEAT] ExceptioinHandler에 로그 코드 삽입

--- src/main/java/net/catsnap/global/result/GlobalBusinessExceptionHandler.java
+++ src/main/java/net/catsnap/global/result/GlobalBusinessExceptionHandler.java
@@ -1,10 +1,13 @@
 package net.catsnap.global.result;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import net.catsnap.global.Exception.BusinessException;
+import net.catsnap.global.log.service.LogService;
 import net.catsnap.global.result.errorcode.CommonErrorCode;
 import net.catsnap.global.result.errorcode.ErrorRepository;
+import org.slf4j.event.Level;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -17,29 +20,34 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 public class GlobalBusinessExceptionHandler {
 
     private final ErrorRepository errorRepository;
+    private final LogService logService;
 
     @ExceptionHandler(value = BusinessException.class)
     public ResponseEntity<ResultResponse<ResultCode>> handleBusinessException(BusinessException e,
-        HttpServletRequest request) {
+        HttpServletRequest request) throws IOException {
+        logService.log(Level.INFO, e, request);
         ResultCode resultCode = errorRepository.getResultCode(e);
         return ResultResponse.of(resultCode);
     }
 
     @ExceptionHandler(value = NoHandlerFoundException.class)
     public ResponseEntity<ResultResponse<ResultCode>> handleNoHandlerFoundException(
-        NoHandlerFoundException e, HttpServletRequest request) {
+        NoHandlerFoundException e, HttpServletRequest request) throws IOException {
+        logService.log(Level.INFO, e, request);
         return ResultResponse.of(CommonErrorCode.NOT_FOUND_API);
     }
 
     @ExceptionHandler(value = MissingServletRequestParameterException.class)
     public ResponseEntity<ResultResponse<ResultCode>> handleMissingServletRequestParameterException(
-        MissingServletRequestParameterException e, HttpServletRequest request) {
+        MissingServletRequestParameterException e, HttpServletRequest request) throws IOException {
+        logService.log(Level.INFO, e, request);
         return ResultResponse.of(CommonErrorCode.MISSING_REQUEST_PARAMETER);
     }
 
     @ExceptionHandler(value = HttpMessageNotReadableException.class)
     public ResponseEntity<ResultResponse<ResultCode>> handleHttpMessageNotReadableException(
-        HttpMessageNotReadableException e, HttpServletRequest request) {
+        HttpMessageNotReadableException e, HttpServletRequest request) throws IOException {
+        logService.log(Level.INFO, e, request);
         return ResultResponse.of(CommonErrorCode.INVALID_REQUEST_BODY);
     }
 }
