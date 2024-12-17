90fa3f8c13ef3fbcb176bce6b964f40c3fcd4018
redblackblossom <physicsdev1029@gmail.com>
2024-12-17 19:36:47 +0900

    [FEAT]  HttpRequest Wrapper를 적용하는 필터 등록

---
+++ src/main/java/net/catsnap/global/filter/RequestWrappingFilter.java
@@ -0,0 +1,23 @@
+package net.catsnap.global.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+@Component
+public class RequestWrappingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+
+        ContentCachingRequestWrapper wrappingRequest = new ContentCachingRequestWrapper(request);
+
+        filterChain.doFilter(wrappingRequest, response);
+    }
+}
