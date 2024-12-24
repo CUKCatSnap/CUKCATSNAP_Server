072602cfd12e4e45cbcc9ee5e0c69e71f951684f
redblackblossom <physicsdev1029@gmail.com>
2024-12-24 21:47:29 +0900

    [FEAT] 주소 엔티티 추가

---
+++ src/main/java/net/catsnap/domain/reservation/entity/CityLevel.java
@@ -0,0 +1,32 @@
+package net.catsnap.domain.reservation.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Point;
+
+@Entity
+@Table(name = "city_level")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CityLevel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+    private String CityName;
+
+    @Column(columnDefinition = "geometry(Point, 4326)")
+    private Point center;
+}
