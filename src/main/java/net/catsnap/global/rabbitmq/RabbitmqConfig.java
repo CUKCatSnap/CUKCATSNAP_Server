de61d65e08381149bf9a04ccd6d9f0abd2a92d8b
redblackblossom <physicsdev1029@gmail.com>
2025-04-09 22:03:36 +0900

    [FEAT] 외부 API 호출 예외 발생시 재시도 로직 구현:

--- src/main/java/net/catsnap/global/rabbitmq/RabbitmqConfig.java
+++ src/main/java/net/catsnap/global/rabbitmq/RabbitmqConfig.java
@@ -2,10 +2,12 @@ package net.catsnap.global.rabbitmq;
 
 import net.catsnap.domain.reservation.rabbitmq.RabbitmqAddressRequestReceiver;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -37,6 +39,13 @@ public class RabbitmqConfig {
         container.setQueueNames(ADDRESS_REQUEST_QUEUE);
         container.setMessageListener(listenerAdapter);
         container.setConcurrentConsumers(30);
+        container.setAdviceChain(
+            RetryInterceptorBuilder.stateless()
+                .maxAttempts(3)
+                .backOffOptions(1000, 2.0, 10000)
+                .recoverer(new RejectAndDontRequeueRecoverer())
+                .build()
+        );
         return container;
     }
 }
