449dc038f817e1ef4404a770aa23063328a24bb0
redblackblossom <physicsdev1029@gmail.com>
2025-04-09 22:03:04 +0900

    [FEAT] 센더 리시버 구현

--- src/main/java/net/catsnap/domain/reservation/rabbitmq/RabbitmqAddressRequestSender.java
+++ src/main/java/net/catsnap/domain/reservation/rabbitmq/RabbitmqAddressRequestSender.java
@@ -1,6 +1,7 @@
 package net.catsnap.domain.reservation.rabbitmq;
 
 import lombok.RequiredArgsConstructor;
+import net.catsnap.global.rabbitmq.RabbitmqConfig;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +11,8 @@ public class RabbitmqAddressRequestSender implements AddressRequestSender {
 
     private final RabbitTemplate rabbitTemplate;
 
-
     @Override
     public void SendRequestAddress(Long reservationId) {
-
+        rabbitTemplate.convertAndSend(RabbitmqConfig.ADDRESS_REQUEST_QUEUE, reservationId);
     }
-
 }
