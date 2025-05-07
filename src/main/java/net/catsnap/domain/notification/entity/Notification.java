d686ce445027ca1b004c55844f66dd7cf17e930c
redblackblossom <physicsdev1029@gmail.com>
2025-05-07 21:23:28 +0900

    [FEAT] 이넘 에노테이션 추가

--- src/main/java/net/catsnap/domain/notification/entity/Notification.java
+++ src/main/java/net/catsnap/domain/notification/entity/Notification.java
@@ -2,6 +2,8 @@ package net.catsnap.domain.notification.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -34,6 +36,7 @@ public class Notification extends BaseTimeEntity {
     @JoinColumn(name = "receiver_id")
     private User receiver;
 
+    @Enumerated(EnumType.STRING)
     private NotificationType notificationType;
     private String title;
     private String content;
