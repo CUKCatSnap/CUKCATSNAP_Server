66d31f41447db1e1d45eb1c0f6c0d2d4947d2518
redblackblossom <physicsdev1029@gmail.com>
2025-05-24 23:05:16 +0900

    [FEAT] 작가의 자기소개 엔티티 ddl 추가

---
+++ src/main/resources/db/migration/V0_0_7__add_photographer_introduction.sql
@@ -0,0 +1,10 @@
+CREATE TABLE photographer_introduction
+(
+    id              BIGSERIAL PRIMARY KEY,
+    photographer_id BIGINT UNIQUE,
+    content         TEXT,
+    created_at      TIMESTAMP(6) NOT NULL,
+    updated_at      TIMESTAMP(6) NOT NULL,
+    CONSTRAINT fk_introduction_photographer__photographer
+        FOREIGN KEY (photographer_id) REFERENCES photographer (id)
+);
