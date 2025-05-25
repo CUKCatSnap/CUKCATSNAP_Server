e01189b92906def04088c46b4d94d6b8cb946631
redblackblossom <physicsdev1029@gmail.com>
2025-05-25 21:25:39 +0900

    [FEAT] 작가으 자기소개를 조회하는 API 개발

--- src/main/java/net/catsnap/domain/user/photographer/controller/PhotographerIntroductionController.java
+++ src/main/java/net/catsnap/domain/user/photographer/controller/PhotographerIntroductionController.java
@@ -6,11 +6,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import net.catsnap.domain.auth.argumentresolver.UserId;
+import net.catsnap.domain.auth.interceptor.AnyUser;
 import net.catsnap.domain.auth.interceptor.LoginPhotographer;
+import net.catsnap.domain.user.photographer.dto.request.PhotographerIntroductionResponse;
 import net.catsnap.domain.user.photographer.service.PhotographerIntroductionService;
 import net.catsnap.global.result.ResultResponse;
 import net.catsnap.global.result.code.CommonResultCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,13 +28,13 @@ public class PhotographerIntroductionController {
 
     private final PhotographerIntroductionService photographerIntroductionService;
 
-    @Operation(summary = "작가가 자기소개를 수정하는 API", description = "작가가 자기소개를 수정하는 API입니다.")
+    @Operation(summary = "작가가 자기소개를 수정하는 API(구현 완료)", description = "작가가 자기소개를 수정하는 API입니다.")
     @ApiResponses({
         @ApiResponse(responseCode = "200 SC002", description = "성공적으로 데이터를 수정했습니다. ")
     })
     @PostMapping("/my")
     @LoginPhotographer
-    public ResponseEntity<ResultResponse<CommonResultCode>> setMyIntroduction(
+    public ResponseEntity<ResultResponse<Void>> setMyIntroduction(
         @UserId
         Long photographerId,
         @RequestParam("introduction") String introduction
@@ -38,5 +42,35 @@ public class PhotographerIntroductionController {
         photographerIntroductionService.updatePhotographerIntroduction(photographerId,
             introduction);
         return ResultResponse.of(CommonResultCode.COMMON_REVISE);
+    }
+
+    @Operation(summary = "작가가 자기소개를 조회하는 API(구현 완료)", description = "작가가 자기소개를 조회하는 API입니다.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200 SC000", description = "성공적으로 데이터를 조회했습니다.")
+    })
+    @GetMapping("/my")
+    @LoginPhotographer
+    public ResponseEntity<ResultResponse<PhotographerIntroductionResponse>> getMyIntroduction(
+        @UserId
+        Long photographerId
+    ) {
+        PhotographerIntroductionResponse response = photographerIntroductionService.getPhotographerIntroduction(
+            photographerId);
+        return ResultResponse.of(CommonResultCode.COMMON_LOOK_UP, response);
+    }
+
+    @Operation(summary = "사용자가 작가의 자기소개를 조회하는 API(구현 완료)", description = "사용자가 작가가 자기소개를 조회하는 API입니다.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200 SC000", description = "성공적으로 데이터를 조회했습니다.")
+    })
+    @GetMapping("/{photographerId}")
+    @AnyUser
+    public ResponseEntity<ResultResponse<PhotographerIntroductionResponse>> getPhotographerIntroduction(
+        @PathVariable("photographerId")
+        Long photographerId
+    ) {
+        PhotographerIntroductionResponse response = photographerIntroductionService.getPhotographerIntroduction(
+            photographerId);
+        return ResultResponse.of(CommonResultCode.COMMON_LOOK_UP, response);
     }
 }
--- src/main/java/net/catsnap/domain/user/photographer/service/PhotographerIntroductionService.java
+++ src/main/java/net/catsnap/domain/user/photographer/service/PhotographerIntroductionService.java
@@ -1,6 +1,7 @@
 package net.catsnap.domain.user.photographer.service;
 
 import lombok.RequiredArgsConstructor;
+import net.catsnap.domain.user.photographer.dto.request.PhotographerIntroductionResponse;
 import net.catsnap.domain.user.photographer.entity.Photographer;
 import net.catsnap.domain.user.photographer.entity.PhotographerIntroduction;
 import net.catsnap.domain.user.photographer.repository.PhotographerIntroductionRepository;
@@ -33,5 +34,14 @@ public class PhotographerIntroductionService {
 
         photographerIntroduction.updateIntroduction(introduction);
         photographerIntroductionRepository.save(photographerIntroduction);
+    }
+
+    @Transactional(readOnly = true)
+    public PhotographerIntroductionResponse getPhotographerIntroduction(Long photographerId) {
+        PhotographerIntroduction photographerIntroduction = photographerIntroductionRepository.findByPhotographerId(
+                photographerId)
+            .orElseThrow(() -> new ResourceNotFoundException("해당 사진작가의 자기소개가 존재하지 않습니다."));
+
+        return PhotographerIntroductionResponse.from(photographerIntroduction);
     }
 }
