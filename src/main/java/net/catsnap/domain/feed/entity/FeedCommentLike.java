2d932747986ff576fecf0372a407272b7fe35924
redblackblossom <physicsdev1029@gmail.com>
2025-06-22 15:38:24 +0900

    [FEAT] 피드의 댓글에 좋아요를 누르는 엔티티를 추가

---
+++ src/main/java/net/catsnap/domain/feed/entity/FeedCommentLike.java
@@ -0,0 +1,37 @@
+package net.catsnap.domain.feed.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import net.catsnap.domain.user.entity.User;
+
+@Entity
+@Table(name = "feed_comment_like")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeedCommentLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "feed_comment_like_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User likeUser;
+
+    @ManyToOne
+    @JoinColumn(name = "feed_comment_id")
+    private FeedComment feedComment;
+}
