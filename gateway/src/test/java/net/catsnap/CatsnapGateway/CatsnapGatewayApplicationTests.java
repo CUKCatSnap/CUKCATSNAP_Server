5827cc88304c2ccd7164c604a702961e5be90f3f
redblackblossom <physicsdev1029@gmail.com>
2025-12-07 19:22:54 +0900

    [FEAT] 게이트웨이 서버 초기 설정

---
+++ gateway/src/test/java/net/catsnap/CatsnapGateway/CatsnapGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.catsnap.CatsnapGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsnapGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
