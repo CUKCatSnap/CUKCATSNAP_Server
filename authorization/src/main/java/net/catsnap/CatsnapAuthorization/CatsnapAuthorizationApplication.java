732c06054a9f5b00d3d79f1317bd8076ebdd1485
redblackblossom <physicsdev1029@gmail.com>
2025-12-09 16:48:05 +0900

    [FEAT] 인증 서버 초기 설정

---
+++ authorization/src/main/java/net/catsnap/CatsnapAuthorization/CatsnapAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package net.catsnap.CatsnapAuthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsnapAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsnapAuthorizationApplication.class, args);
+	}
+
+}
