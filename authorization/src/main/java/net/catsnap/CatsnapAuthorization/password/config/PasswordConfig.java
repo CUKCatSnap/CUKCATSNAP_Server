25f48accc14103279309c0714b195f825a2673f0
redblackblossom <physicsdev1029@gmail.com>
2025-12-12 16:16:11 +0900

    [REFACT] password config 파일 경로 변경

--- authorization/src/main/java/net/catsnap/CatsnapAuthorization/password/config/PasswordConfig.java
+++ authorization/src/main/java/net/catsnap/CatsnapAuthorization/password/config/PasswordConfig.java
@@ -1,4 +1,4 @@
-package net.catsnap.CatsnapAuthorization.config;
+package net.catsnap.CatsnapAuthorization.password.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
