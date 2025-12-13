ce6b86930e81d605e6626b41cfb23448dc859a5d
redblackblossom <physicsdev1029@gmail.com>
2025-12-13 19:57:40 +0900

    [FEAT] 공통 응답 로직 개발

---
+++ authorization/src/main/java/net/catsnap/shared/infrastructure/web/response/code/CommonResultCode.java
@@ -0,0 +1,46 @@
+package net.catsnap.shared.infrastructure.web.response.code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import net.catsnap.shared.infrastructure.web.response.ResultCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+/**
+ * 공통 결과 코드
+ * <p>
+ * 모든 도메인에서 공통적으로 사용하는 성공 응답 코드를 정의합니다.
+ */
+@Getter
+@RequiredArgsConstructor
+public enum CommonResultCode implements ResultCode {
+
+    /**
+     * 데이터 조회 성공
+     */
+    COMMON_LOOK_UP(HttpStatus.OK, "SA000", "성공적으로 데이터를 조회했습니다."),
+
+    /**
+     * 데이터 생성 성공
+     */
+    COMMON_CREATE(HttpStatus.CREATED, "SA001", "성공적으로 데이터를 생성했습니다."),
+
+    /**
+     * 데이터 수정 성공
+     */
+    COMMON_REVISE(HttpStatus.OK, "SA002", "성공적으로 데이터를 수정했습니다."),
+
+    /**
+     * 데이터 삭제 성공
+     */
+    COMMON_DELETE(HttpStatus.OK, "SA003", "성공적으로 데이터를 삭제했습니다."),
+
+    /**
+     * 작업 완료 (데이터 없음)
+     */
+    COMMON_NO_CONTENT(HttpStatus.NO_CONTENT, "SA004", "요청이 성공적으로 처리되었습니다.");
+
+    private final HttpStatusCode httpStatus;
+    private final String code;
+    private final String message;
+}
