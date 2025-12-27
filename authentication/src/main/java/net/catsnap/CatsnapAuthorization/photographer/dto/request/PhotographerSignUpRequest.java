c66fe5e7de21fe248c8006d879bc8b370e610370
redblackblossom <physicsdev1029@gmail.com>
2025-12-27 22:38:39 +0900

    [REFACT] 전화번호도 엄격한 체크 완료

--- authentication/src/main/java/net/catsnap/CatsnapAuthorization/photographer/dto/request/PhotographerSignUpRequest.java
+++ authentication/src/main/java/net/catsnap/CatsnapAuthorization/photographer/dto/request/PhotographerSignUpRequest.java
@@ -1,7 +1,6 @@
 package net.catsnap.CatsnapAuthorization.photographer.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record PhotographerSignUpRequest(
     @NotBlank(message = "식별자는 필수입니다")
@@ -13,7 +12,7 @@ public record PhotographerSignUpRequest(
     @NotBlank(message = "이름은 필수입니다")
     String name,
 
-    @NotNull(message = "전화번호는 필수입니다")
+    @NotBlank(message = "전화번호는 필수입니다")
     String phoneNumber
 ) {
 
