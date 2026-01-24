7c74e27faf7bb73d7fed3b8cdc49bfc7523348c7
chokyungho <physicsdev1029@gmail.com>
2026-01-24 15:29:04 +0900

    [CHORE] 주석 추가

--- reservation/src/main/java/net/catsnap/CatsnapReservation/event/infrastructure/EventConfig.java
+++ reservation/src/main/java/net/catsnap/CatsnapReservation/event/infrastructure/EventConfig.java
@@ -5,6 +5,12 @@ import net.catsnap.shared.infrastructure.AvroEventDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 이벤트 처리 관련 빈 설정
+ *
+ * <p>Kafka Consumer가 수신한 EventEnvelope의 payload를 도메인 이벤트로 변환할 때 사용됩니다.</p>
+ *
+ */
 @Configuration
 public class EventConfig {
 
