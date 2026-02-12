8b123edd18d3cc16b5f7bded2ec0b0a780636cbb
chokyungho <physicsdev1029@gmail.com>
2026-02-12 10:42:41 +0900

    [TEST] JpaAuditingConfig 설정 추가

--- reservation/src/test/java/net/catsnap/CatsnapReservation/program/infrastructure/repository/ProgramRepositoryTest.java
+++ reservation/src/test/java/net/catsnap/CatsnapReservation/program/infrastructure/repository/ProgramRepositoryTest.java
@@ -5,17 +5,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import net.catsnap.CatsnapReservation.program.domain.Program;
 import net.catsnap.CatsnapReservation.program.fixture.ProgramFixture;
+import net.catsnap.CatsnapReservation.shared.infrastructure.JpaAuditingConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Import;
 
 @DisplayName("ProgramRepository 테스트")
 @DisplayNameGeneration(ReplaceUnderscores.class)
-@EnableJpaAuditing
+@Import(JpaAuditingConfig.class)
 @SuppressWarnings("NonAsciiCharacters")
 @DataJpaTest
 class ProgramRepositoryTest {
